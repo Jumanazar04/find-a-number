f5c22d7ecc77d13cdb58d8bdccf7916e95605a1a
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-01-17 17:00:26 +0500

    so

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('#form');
 const input = document.getElementById('number');
 const text = document.getElementById('text');
+const body = document.querySelector('body');
+const btn = document.getElementById('generate');
 
-
-
-
-
-
+btn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    genareteNumber()
+})
 
 function genareteNumber (){
     let num = Math.floor(Math.random()* 11)
@@ -21,7 +22,7 @@ form.addEventListener("submit", (e)=>{
     let valInp = input.value
     if(valInp == hiddenNumber){
         text.textContent = 'You are won!!!!'
-        text.style.background = 'green'
+        body.style.background = 'green'
     }else if (valInp < hiddenNumber){
         text.textContent = 'siz oylagan son yashiringan sondan kichkina'
     }else if (valInp > hiddenNumber){
@@ -29,6 +30,8 @@ form.addEventListener("submit", (e)=>{
     }
 
     console.log(hiddenNumber);
+
+    form.reset()
 })
 
 console.log(genareteNumber());
